bc069dc4cc5606ead9cff1f2433f34c7ac655da2
Niclas Lovsjö <niclo06@liu.se>
2018-04-06 13:21:34 +0200

    .

--- find_latent.R
+++ find_latent.R
@@ -4,26 +4,45 @@ library(data.table)
 
 #Constraints for constrOptim
 #On the form: ui%*%theta >= ci
+
+# 
+
+#----------------------------------------------------------------
+#Test-datasets
 # test_data <- matrix(rpois(100,8),nrow=10,ncol=10)
 # diag(test_data)<-0
-# 
+
 test_data <- fread("C:/Users/niclo06/Dropbox/valued null networks/data2/7cont_nodiag.txt")
 test_data <- as.matrix(test_data[,-1])
 
+library(igraphdata)
+library(igraph)
+data("karate")
+test_data <- as_adjacency_matrix(karate)
+test_data <- as.matrix(test_data)
+
+#-----------------------------------------------------------------
+
+
 find_latent <- function(data,
-                        init_values = rep(0.5,length(data)),
-                        type="weighted_directed"
-                        ){
+                        type="weighted_directed"){
   
-  if(is.matrix(data)){
-    data <- as.vector(c(apply(data,1,sum),apply(data,2,sum)))
-  } else {cat("Assuming data is a vector of in-degrees and out-degrees for each node.")}
-  
-  
-  N <- length(data)
+
   
   #---------------------------------------------------------------
   if(type == "weighted_directed"){
+    
+    
+    if(is.matrix(data)){
+      data <- as.vector(c(apply(data,1,sum),apply(data,2,sum)))
+    } else {cat("Assuming data is a vector of in-degrees and out-degrees for each node.")}
+    
+    
+    init_values = rep(0.5,length(data))
+    
+    N <- length(data)
+    n <- N/2
+    
     
     #Set up constraints for constrOptim
     #This is simply a design matrix that follows
@@ -54,8 +73,18 @@ find_latent <- function(data,
                           method="Nelder-Mead",
                           control=list(maxit=100000))
     
+    result <- data.table(indeg=result$par[1:n],outdeg=result$par[(n+1):N])
+    }
+  else if(type == "binary_undirected"){
     
-  } else if(type == "binary_undirected"){
+    if(is.matrix(data)){
+      data <- as.vector(c(apply(data,1,sum)))
+    } else {cat("Assuming data is a vector of in-degrees and out-degrees for each node.")}
+    
+    
+    init_values = rep(0.5,length(data))
+    
+    N <- length(data)
     
     #Unconstrained optim, instead taking exponentials to force latent>0
     result <- optim(par = init_values,
@@ -63,16 +92,10 @@ find_latent <- function(data,
                     fn=undirected_binary_lkl,
                     method="CG",
                     control=list(maxit=1000))
-  }
-
-  
-  if(result$convergence==0){
-    cat("Converged!\n")
-    cat("Used", result$outer.iterations, "iterations.\n")
+    result <- data.table(degree = exp(result$par))
   }
   
-  
-  return(result$par)
+  return(result)
 }
 
-first <- find_latent(data = test_data)
+first <- find_latent(data = test_data,type = "weighted_directed")
